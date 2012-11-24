8fa8d8015491c3ed394def955497f882a7e68a6e
lyyder <lyyder@gmail.com>
2012-11-24 12:54:58 +0200

    parkingmeter representation changed to json

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -2,11 +2,21 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.json.Json._
 
 object Api extends Controller {
   
   def parkingmeter = Action {
-    Ok("parkingmeter")
+    val meter = toJson(
+      Map(
+        "id" -> toJson("dummy meter id"),
+        "lat" -> toJson(22.2),
+        "lng" -> toJson(33.3)
+      )
+    )
+
+    Ok(meter)
   }
   
 }
