0718e476ad41ce269624b231467a3c53309bd16f
lyyder <lyyder@gmail.com>
2012-11-27 21:48:08 +0200

    Added play-salat plugin.

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      // play-salat
+      "se.radley" %% "play-plugins-salat" % "1.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // play-salat
+      routesImport += "se.radley.plugin.salat.Binders._",
+      templatesImport += "org.bson.types.ObjectId"
     )
 
 }
