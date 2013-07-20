f3b89fb019d9a58773fd04776fc222de360e3cfc
lyyder <lyyder@gmail.com>
2013-07-20 20:04:50 +0300

    Added resolving and drawing of the closest parkingmeter.

--- public/javascripts/park.js
+++ public/javascripts/park.js
@@ -1,17 +1,54 @@
+var park = (function () {
+    var map;
 
-park = (function() {
-    
+    function updateClosesParkingmeter() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    map.setView(new L.LatLng(position.coords.latitude, position.coords.longitude), 15);
+                    $.get("/parkingmeters",
+                        {
+                            lng: position.coords.longitude,
+                            lat: position.coords.latitude,
+                            maxDistance: 1000,
+                            limit: 1
+                        },
+                        function (data) {
+                            drawOnMap(data[0]);
+                        });
+                },
+                function () {
+                    // location error function
+                }
+            );
+        }
+    }
+
+    function drawOnMap(parkingmeter) {
+        L.mapbox.markerLayer(
+            {
+                type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                    coordinates: [parkingmeter.lng, parkingmeter.lat]
+                },
+                properties: {
+                    title: parkingmeter.address
+                }
+            }
+
+        ).addTo(map);
+    }
+
     return {
         init: function () {
-            console.log("park.init");
-            if (navigator.geolocation) {
-               // navigator.geolocation.getCurrentPosition()
-                console.log("geolocation");
-            }
-        
+            map = L.mapbox.map('map', 'lyyder.map-t2vxuiud');
+            map.on('ready', function () {
+                updateClosesParkingmeter();
+            });
+
 
         }
-
     };
 }());
 
